cef9ce0c4b7934c910bc828ec55eeb87c6d8a371
Johnny Kreitzman <kreitzka@gmail.com>
2019-09-15 14:55:56 -0500

    started basic layout of page created questions and corresponding array

---
+++ assets/javascript/trivia.js
@@ -0,0 +1,31 @@
+var question1 = {
+    text: "What is Prince's real name?",
+    answer: "Prince Rogers Nelson",
+    wrong: ["Roger Nelson", "Nelson Rogers", "Nelson King"],
+    correct = false,
+}
+
+var question2 = {
+    text: "What was the name for Prince's symbol? (when he went by 'The Artist formerly known as Prince')",
+    answer: "Love Symbol #2",
+    wrong: ["Love Potion Number 9", "The Artist", "Prince Symbol", "It didn't have a name."],
+    correct = false,
+}
+
+var question3 = {
+    text: "Which of the following songs wasn't written by Prince?",
+    answer: "I Wanna Dance With Somebody",
+    wrong: ["Jungle Love", "Nothing Compares 2 U", "Manic Monday"],
+    correct = false,
+}
+
+var question4 = {
+    text: "In 2017, Pantone named a shade of purple for Prince.  What did they name it?",
+    answer: "Love Symbol #2",
+    wrong: ["Purple Rain", "Love Potion #9", "Purple Reign"],
+    correct = false,
+}
+
+var questionsArray = [question1, question2, question3, question4];
+var questionsLength = questionsArray.length;
+
